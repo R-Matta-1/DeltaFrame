6af361a8e881ec8efd661390b9636f42316071c7
R-Matta-1 <raphaelmatta2007@gmail.com>
2024-10-03 13:16:46 -0400

    vis started

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 console.log("hello Setup\n world")
+
+
+var nodes = new vis.DataSet([
+    {id: 0, label:"Start"},
+    {id:2 , label:"f(x)"},
+    {id: 1, label:"Final Product"}
+]);
+
+var edges = new vis.DataSet([
+    {from:0 ,to:2},
+    {from:2, to:1}
+]);
+
+var container = document.getElementById('graphNetwork');
+
+var data = {
+    nodes:nodes,
+    edges:edges
+};
+var network = new vis.Network(container, data, {});
