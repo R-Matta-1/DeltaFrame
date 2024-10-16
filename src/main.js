aa714ae417e0e9f3799a8bb1c3d755df37938ad2
R-Matta-1 <raphaelmatta2007@gmail.com>
2024-10-15 20:41:44 -0400

    node altering & edge adding

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 const FakeConsole = document.getElementById("trash")
 
+const EdgeCreationToggleBtn = document.getElementById("EdgeCreationToggleBtn");
+var uniqueIdNumber=0
+var EdgeCreationMode = false
 
 var cy = cytoscape({
     container: document.getElementById('cy'), // container to render in
@@ -27,19 +30,74 @@ var cy = cytoscape({
 
     layout: { name: 'grid', rows: 1}
   });
+  // selection box is a useless eye sore (unless I think of a use then it's just an eyesore)
+  cy.boxSelectionEnabled(false)
 
-var num=0
-function CreateNode(){
-  cy.add({
-    data:{id:'another'+(++num)},
+  
+var SelectedNode = null;
+  // when on EdgeCreationMode sleecting a node should create an invisible node that follows the mouse until released unto another node
+cy.on("cxttap" , "node"  , function(evt){
+  CurrentNode = evt.target
+
+  if (SelectedNode == null) { 
+    // if thier is no previously selected node, make that this node
+    SelectedNode = CurrentNode;
+    console.log("node selected: "+SelectedNode.id())
+    return;
+  } 
+   if( SelectedNode.id() !=CurrentNode.id()  
+   && cy.elements(`edge[source = "${SelectedNode.id()}"][target = "${CurrentNode.id()}"]`).length == 0
+  &&  cy.elements(`edge[source = "${CurrentNode.id()}"][target = "${SelectedNode.id()}"]`).length == 0){ 
+    // thier IS a node, if we are not looping it  AND thier is no identicle edge, create the edge
+  
+        cy.add({group:"edges" ,
+          data:{
+            source:SelectedNode.id(),
+            target:CurrentNode.id()
+          }})
+          console.log( 'connection from:  ' + SelectedNode.id() + " to"+ CurrentNode.id() );
+          SelectedNode = null;
+
+    
+  }
+})
+
+cy.on("cxttap" , "edge"  , function(evt){
+  cy.remove(evt.target)
+})
+  
+  
+document.addEventListener('click',function(event){
+  console.log("click")
+  SelectedNode = null;
+})
+
+  function CreateNode(){ // add custom name, custom metadata
+    cy.add({
+    data:{id:'another'+ (++uniqueIdNumber)}, // unique id
     position:averageGridCenter()
    }) //position =  average pos of all obj
 }
+
+
 
 averageGridCenter = () => {
   for (let i =0 ; i < cy.nodes().length;i++){
     console.log(cy.nodes()[i].position("x"))
   }
+let avgX =0;
+let avgY =0;
 
-  return {x:0,y:0}
+cy.nodes().forEach(element => {
+  avgX += element.position().x;
+  avgY += element.position().y;
+});
+let length  = cy.nodes().length;
+
+avgX /= length;
+avgY /= length;
+
+  return {x:avgX , y:avgY-50}
 }
+
+
