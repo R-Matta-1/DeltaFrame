2dfe5716b383ae4a8a1c074f1b9e0613ddd9bfcb
R-Matta-1 <raphaelmatta2007@gmail.com>
2024-10-16 00:54:30 -0400

    sidebar and todo

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ const FakeConsole = document.getElementById("trash")
 
 const EdgeCreationToggleBtn = document.getElementById("EdgeCreationToggleBtn");
 var uniqueIdNumber=0
-var EdgeCreationMode = false
+const mouseFollowerNodeId = `MouseFollower${Math.random()}`
 
 var cy = cytoscape({
     container: document.getElementById('cy'), // container to render in
     elements: [ // list of graph elements to start with
-      {data: { id: 'Start' }}, //node
-      {data: { id: 'Final' }}, //node
+      {data: { id: 'Start' }, style:{backgroundColor:"#D81E5B", shape:"hexagon"}}, //node
+      {data: { id: 'Final' }, style:{backgroundColor:"#D81E5B", shape:"hexagon"}}, //node
       {data: { id: 'ab', source: 'Start', target: 'Final' }} // edge
     ],
   style: [ {
@@ -20,7 +20,7 @@ var cy = cytoscape({
       {
         selector: 'edge',
         style: {
-          'width': 3,
+          'width': 4,
           'line-color': '#aaa',
           'target-arrow-color': '#444',
           'target-arrow-shape': 'triangle',
@@ -32,34 +32,33 @@ var cy = cytoscape({
   });
   // selection box is a useless eye sore (unless I think of a use then it's just an eyesore)
   cy.boxSelectionEnabled(false)
-
+ 
   
 var SelectedNode = null;
-  // when on EdgeCreationMode sleecting a node should create an invisible node that follows the mouse until released unto another node
-cy.on("cxttap" , "node"  , function(evt){
-  CurrentNode = evt.target
+  cy.on("cxttap", "node", function(evt) {
+    CurrentNode = evt.target
 
-  if (SelectedNode == null) { 
-    // if thier is no previously selected node, make that this node
-    SelectedNode = CurrentNode;
-    console.log("node selected: "+SelectedNode.id())
-    return;
-  } 
-   if( SelectedNode.id() !=CurrentNode.id()  
-   && cy.elements(`edge[source = "${SelectedNode.id()}"][target = "${CurrentNode.id()}"]`).length == 0
-  &&  cy.elements(`edge[source = "${CurrentNode.id()}"][target = "${SelectedNode.id()}"]`).length == 0){ 
-    // thier IS a node, if we are not looping it  AND thier is no identicle edge, create the edge
-  
-        cy.add({group:"edges" ,
-          data:{
-            source:SelectedNode.id(),
-            target:CurrentNode.id()
-          }})
-          console.log( 'connection from:  ' + SelectedNode.id() + " to"+ CurrentNode.id() );
-          SelectedNode = null;
+    if (SelectedNode == null) {
+        // if thier is no previously selected node, make that this node
+        BeginEdgeSelection(CurrentNode,evt.position.x,evt.position.y)
+        return;
+    }
+    if (SelectedNode.id() != CurrentNode.id() &&
+        cy.elements(`edge[source = "${SelectedNode.id()}"][target = "${CurrentNode.id()}"]`).length == 0 &&
+        cy.elements(`edge[source = "${CurrentNode.id()}"][target = "${SelectedNode.id()}"]`).length == 0) {
+        // thier IS a node, if we are not looping it  AND thier is no identicle edge, create the edge
 
-    
-  }
+        cy.add({
+            group: "edges",
+            data: {
+                source: SelectedNode.id(),
+                target: CurrentNode.id()
+            }
+        })
+        console.log('connection from:  ' + SelectedNode.id() + " to" + CurrentNode.id());
+		EndEdgeSelection()
+
+    }
 })
 
 cy.on("cxttap" , "edge"  , function(evt){
@@ -69,10 +68,11 @@ cy.on("cxttap" , "edge"  , function(evt){
   
 document.addEventListener('click',function(event){
   console.log("click")
-  SelectedNode = null;
+  EndEdgeSelection()
+
 })
 
-  function CreateNode(){ // add custom name, custom metadata
+function CreateNode(){ // add custom name, custom metadata
     cy.add({
     data:{id:'another'+ (++uniqueIdNumber)}, // unique id
     position:averageGridCenter()
@@ -81,8 +81,33 @@ document.addEventListener('click',function(event){
 
 
 
+function BeginEdgeSelection(node,x,y) {
+	SelectedNode = node;
+        console.log("node selected: " + SelectedNode.id())
+		cy.add({group:"nodes", 
+			data:{id:mouseFollowerNodeId },style:{ visibility: 'hidden'},
+			position:{x:x , y:y},
+			
+		})
+		cy.add({group:"edges", data:{source: SelectedNode.id(), target: mouseFollowerNodeId}})
+
+		cy.on("vmousemove" , function (evt) {
+			console.log("move")
+			if (cy.nodes(`[id = "${mouseFollowerNodeId}"]`).length !=0) {
+				cy.nodes(`[id = "${mouseFollowerNodeId}"]`).position({x:evt.position.x , y:evt.position.y})
+			}
+		})
+}
+function EndEdgeSelection(){
+	console.log("SelectedNode reset")
+	SelectedNode = null
+	cy.remove(`node[id = "${mouseFollowerNodeId}"]`);
+	cy.remove(`edge[target = "${mouseFollowerNodeId}"]`);
+	cy.removeListener("vmousemove")
+}
+
 averageGridCenter = () => {
-  for (let i =0 ; i < cy.nodes().length;i++){
+  for (let i =0 ; i < cy.nodes().length; i++){
     console.log(cy.nodes()[i].position("x"))
   }
 let avgX =0;
