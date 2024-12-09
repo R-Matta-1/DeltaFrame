de9f1613aa7b133bd90f4239471118fc4a49d8c5
Raphael Matta <125845104+R-Matta-1@users.noreply.github.com>
2024-12-09 18:53:39 +0000

    DivHandle now controls it's id to include it's type

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,167 @@
-import  './index.css';
-import { applyNodeChanges
-	 ,applyEdgeChanges,
-	 Background, 
-	 ReactFlow, 
-	 useEdgesState, 
-	 useNodesState, 
-	 addEdge, 
-	 useReactFlow, 
-	 ReactFlowProvider, 
-	 Position } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
+import "./index.css";
+import {
+  applyNodeChanges,
+  applyEdgeChanges,
+  Background,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+  addEdge,
+  useReactFlow,
+  ReactFlowProvider,
+  Position,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
-import {InitalNodes, nodeTypes} from "./nodes/index.jsx";
-import { useState, useCallback } from 'react';
+import { InitalNodes, nodeTypes } from "./nodes/index.jsx";
+import { useState, useCallback } from "react";
 
 let id = 0;
 const origionalId = () => `NodeId:${id++}`;
 
-function Sidebar(props){
+function Sidebar(props) {
+  return (
+    <div className="sidebar">
+      <h1>Delta Frame</h1>
+      <hr />
+      {Object.entries(nodeTypes).map(([keys]) => (
+        <div
+          draggable
+          key={keys.keys}
+          onDragStart={(e) => {
+            props.onNodeDrag(e, { keys });
+          }}
+          className="DraggingDiv"
+        >
+          <h4 className="NodeName">{keys}</h4>
 
-    return(
-<div className="sidebar">
-    <h1>Delta Frame</h1>
-    <hr />
-{Object.entries(nodeTypes).map(([keys,])=>(
-	<div 
-		draggable 
-		key={keys.keys} 
-		onDragStart = {(e)=>{props.onNodeDrag(e,{keys})}} 
-		className= "DraggingDiv">
-
-<h4 className='NodeName'>{keys}</h4>
-
-<hr ></hr>
-
-	</div>
-))}
+          <hr></hr>
+        </div>
+      ))}
     </div>
-    )}
-    
- 
- function App() {
-    const [nodes, setNodes] = useNodesState(InitalNodes);
-    const [edges, setEdges] = useEdgesState([]);
-    const [DraggedType, setDraggedType] =useState("");
-    const {screenToFlowPosition, getNodes, getEdges } = useReactFlow()
+  );
+}
 
-    const onNodesChange = useCallback(
-        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-        [],
+function App() {
+  const [nodes, setNodes] = useNodesState(InitalNodes);
+  const [edges, setEdges] = useEdgesState([]);
+  const [DraggedType, setDraggedType] = useState("");
+  const { screenToFlowPosition, getNodes, getEdges, ha } = useReactFlow();
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+
+  const onEdgesChange = useCallback(
+    (changes) =>
+      setEdges((eds) => {
+        applyEdgeChanges(changes, eds);
+      }),
+    []
+  );
+
+  const onConnect = useCallback((newEdge) => {
+    const newEdges = edges.concat(newEdge);
+
+    const repeatedTargetEdges = newEdges.filter((eds) => {
+      return (
+        eds.targetHandle == newEdge.targetHandle && eds.target == newEdge.target
       );
+    });
+    console.log(repeatedTargetEdges.length);
 
-    const onEdgesChange = useCallback(
-        (changes) => setEdges((eds) => {applyEdgeChanges(changes, eds)}),
-        [],
-      );
-	  
-     const onConnect = useCallback(
-        (newEdg) => {
-          const newEdges = edges.concat(newEdg);
-          const repeatedTargetEdges = newEdges.filter((eds)=>{return eds.targetHandle == newEdg.targetHandle && eds.target == newEdg.target})
-          console.log(repeatedTargetEdges.length)
-          if (repeatedTargetEdges.length > 1) {
-            console.log("more then edge targeting this HANDLE")            
-         } else {
-          setEdges((eds) => addEdge(newEdg,eds))
-         }
-        })
-
-    const onNodeDrag = (event,keys) =>{
-		console.log("drag: "+keys.keys)
-		setDraggedType(keys.keys)
+    if (repeatedTargetEdges.length > 1) {
+      console.log("already another edge else with this as a targert");
+      return;
     }
 
-    const onDragOver = (event) =>{
-    
-      event.preventDefault();
-      console.log(DraggedType)
-    }
+    newEdges.forEach((edge) => {
+      console.log(edge);
+    });
+    console.log(newEdges.length);
 
-    const onDrop = (event) => {
+    setEdges((eds) => addEdge(newEdge, eds));
+  });
+
+  const onNodeDrag = (event, keys) => {
+    console.log("drag: " + keys.keys);
+    setDraggedType(keys.keys);
+  };
+
+  const onDragOver = (event) => {
     event.preventDefault();
-    const newPosition = screenToFlowPosition({x:event.clientX, y:event.clientY })
-    const newNode = {
-        id:origionalId(),
-        type:DraggedType,
-        position:newPosition
-    }
+    console.log(DraggedType);
+  };
 
-	const nodeAtSamePosition = getNodes().filter(
-        node => node.position.x == newPosition.x && node.position.y == newPosition.y
-	)
-      
-      if(nodeAtSamePosition.length == 0 && DraggedType in nodeTypes){ // solves double action problem
-        setNodes((nds) => nds.concat(newNode));
-      }
+  const onDrop = (event) => {
+    event.preventDefault();
+    const newPosition = screenToFlowPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+    const newNode = {
+      id: origionalId(),
+      type: DraggedType,
+      position: newPosition,
     };
 
-	const onNodeDragStop = (event, node, nodes) => {
-		const MouseOnSidebar = event.clientX / window.innerWidth < 0.2 ; 
-		console.log( event.clientX / window.innerWidth)
-		if(MouseOnSidebar){
-			setNodes(getNodes().filter(nds => nds.id != node.id))
+    const nodeAtSamePosition = getNodes().filter(
+      (node) =>
+        node.position.x == newPosition.x && node.position.y == newPosition.y
+    );
 
-		}
-	
-	}
+    if (nodeAtSamePosition.length == 0 && DraggedType in nodeTypes) {
+      // solves double action problem
+      setNodes((nds) => nds.concat(newNode));
+    }
+  };
 
-	const onEdgeClick =(event,edge) =>{
-			setEdges(getEdges().filter(edg => edg.id != edge.id))
-	}
+  const onNodeDragStop = (event, node, nodes) => {
+    const MouseOnSidebar = event.clientX / window.innerWidth < 0.2;
+    console.log(event.clientX / window.innerWidth);
+    if (MouseOnSidebar) {
+      setNodes(getNodes().filter((nds) => nds.id != node.id));
+    }
+  };
 
-   return (
-   <>
-         <Sidebar onNodeDrag = {onNodeDrag} /> 
+  const onEdgeClick = (event, edge) => {
+    setEdges(getEdges().filter((edg) => edg.id != edge.id));
+  };
 
-         <div  className='body'>
-<ReactFlow  
-    nodes={nodes} 
-    onNodesChange={onNodesChange}
-    onEdgesChange={onEdgesChange}
-    onConnect={onConnect}
-    nodeTypes={nodeTypes}
-    edges = {edges}
-    onDropCapture={(e)=>{onDrop(e)}}
-    onDrop ={onDrop}
-    onDragOver ={onDragOver}
-	autoPanOnNodeDrag = {false}
-	onNodeDragStop = {onNodeDragStop}
-	onEdgeClick = {onEdgeClick}
-    fitView 
- >
-	<Background/>
-	 </ReactFlow>
-        </div>
-        </>
-        
-)}
+  return (
+    <>
+      <Sidebar onNodeDrag={onNodeDrag} />
 
-export default function(){
-return <ReactFlowProvider>
-  <App />
-</ReactFlowProvider>
+      <div className="body">
+        <ReactFlow
+          nodes={nodes}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          nodeTypes={nodeTypes}
+          edges={edges}
+          onDropCapture={(e) => {
+            onDrop(e);
+          }}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          autoPanOnNodeDrag={false}
+          onNodeDragStop={onNodeDragStop}
+          onEdgeClick={onEdgeClick}
+          fitView
+        >
+          <Background />
+        </ReactFlow>
+      </div>
+    </>
+  );
+}
+
+export default function () {
+  return (
+    <ReactFlowProvider>
+      <App />
+    </ReactFlowProvider>
+  );
 }
--- src/nodes/index.jsx
+++ src/nodes/index.jsx
@@ -1,126 +1,179 @@
 import { Position, Handle, useHandleConnections } from "@xyflow/react";
-import {useCallback, useState} from "react";
+import { useCallback, useState } from "react";
 
 const MediaTypes = {
-    VIDEO:"video",
-    AUDIO:"audio"
+  VIDEO: "video",
+  AUDIO: "audio",
+};
+
+function DivHandle(props) {
+  const params = { ...props };
+  params.id = params.mediaType + params.type + params.id;
+  const divStyle = {
+    border: "solid black 1px",
+    zIndex: -1,
+    position: "absolute",
+    width: "10px",
+    height: "10px",
+    top: "-2.5px",
+    font: "8px  Verdana black",
+  };
+  if (params.position === Position.Left) {
+    divStyle.left = "-15px";
+  } else {
+    divStyle.right = "-15px";
+  }
+  if (params.type === "target") {
+    divStyle.backgroundColor = "#fcf";
+  } else {
+    divStyle.backgroundColor = "#cfc";
+  }
+
+  return (
+    <>
+      <Handle style={{ zIndex: "100" }} {...params}>
+        <div style={divStyle}>
+          {params.mediaType == MediaTypes.VIDEO ? "V" : null}
+          {params.mediaType == MediaTypes.AUDIO ? "A" : null}
+        </div>
+      </Handle>
+    </>
+  );
 }
 
-function DivHandle(params) {
-    const divStyle = {
-        border:"solid black 1px" , 
-        zIndex:-1,
-        position:"absolute",
-        width:"10px",
-        height:"10px",
-        top:"-2.5px",
-        font: "8px  Verdana black"
+function VideoOutput({ x, y, Data }) {
+  return (
+    <div
+      style={{ justifyContent: "center", width: "360px", height: "175px" }}
+      className="react-flow__node-default"
+    >
+      <video
+        controls
+        src="https://upload.wikimedia.org/wikipedia/commons/d/d9/177_day_spin_up_zonal_velocity.webm"
+      ></video>
 
-    }
-    if(params.position === Position.Left){
-        divStyle.left ="-15px"
-    } else {
-        divStyle.right="-15px"
-    }
-    if(params.type === "target"){
-        divStyle.backgroundColor ="#fcf"
-    } else {
-        divStyle.backgroundColor="#cfc"
-    }
-
-    return<>
-    <Handle style={{zIndex:"100000"}} {...params}>
-        <div style={divStyle} 
-                >
-                    {(params.mediaType == MediaTypes.VIDEO)?"V":null}
-                    {(params.mediaType == MediaTypes.AUDIO)?"A":null}
-
-                </div>
-    </Handle>
-    </>}
-
-function VideoOutput({x,y,Data}) {
-   
-    return(
-    <div style={{ justifyContent:"center", width:"360px" ,height: "175px"}}  className="react-flow__node-default">
-       
-    <video controls src="https://upload.wikimedia.org/wikipedia/commons/d/d9/177_day_spin_up_zonal_velocity.webm" ></video>
-
-    <DivHandle type="target" mediaType = {MediaTypes.VIDEO} id="input" position= {Position.Left}  style={{top:"33%"}}  />
-    <DivHandle type="target" mediaType = {MediaTypes.AUDIO} id="input" position= {Position.Left}  style={{top:"66%"}}  />
-</div>
-    )
+      <DivHandle
+        type="target"
+        id="1"
+        mediaType={MediaTypes.VIDEO}
+        position={Position.Left}
+        style={{ top: "33%" }}
+      />
+      <DivHandle
+        type="target"
+        id="2"
+        mediaType={MediaTypes.AUDIO}
+        position={Position.Left}
+        style={{ top: "66%" }}
+      />
+    </div>
+  );
 }
 
-function VideoDifference({x,y,Data}) {
-   return( <>
-    <div style= {{height: "20%", padding:"0"}} className="react-flow__node-default">
-    <p style={{font:"15px Verdana"}}>Difference</p>
-   
-<DivHandle type="target" id="Input1" position= {Position.Left} mediaType = {MediaTypes.VIDEO} style={{top:"75%"}}  />
+function VideoDifference({ x, y, Data }) {
+  return (
+    <>
+      <div
+        style={{ height: "20%", padding: "0" }}
+        className="react-flow__node-default"
+      >
+        <p style={{ font: "15px Verdana" }}>Difference</p>
 
-<DivHandle type="target" id="Input2" position= {Position.Left} mediaType = {MediaTypes.VIDEO} style={{top:"25%"}} />
+        <DivHandle
+          type="target"
+          id="1"
+          position={Position.Left}
+          mediaType={MediaTypes.VIDEO}
+          style={{ top: "75%" }}
+        />
 
-<DivHandle type="source" id="Output" position= {Position.Right} mediaType = {MediaTypes.VIDEO}   />
+        <DivHandle
+          type="target"
+          id="2"
+          position={Position.Left}
+          mediaType={MediaTypes.VIDEO}
+          style={{ top: "25%" }}
+        />
 
-</div></>)
+        <DivHandle
+          type="source"
+          id="3"
+          position={Position.Right}
+          mediaType={MediaTypes.VIDEO}
+        />
+      </div>
+    </>
+  );
 }
 
 function VideoInput() {
-    const [VideoLink,ChangeVideoLink] = useState('https://media.istockphoto.com/id/1413207061/video/road-traffic-in-delhi-roads.mp4?s=mp4-640x640-is&k=20&c=KnGos4ZVgHxZSV-zGAJk0mWsjR2kLGumoVcKI-PanEw=')
-    
-    function RedirectClick(e) {
-        const HiddenInput = e.target.querySelector("input");
-        if (HiddenInput) {
-            HiddenInput.click()
-        }
-    }
-    function InputAdition(e) {
-        const InputVideoURL = URL.createObjectURL(e.target.files[0])
-        ChangeVideoLink(InputVideoURL)
-    }
-    return(
-    <div 
-        style= {{ justifyContent:"center", width:"300px" ,height: "175px"}} 
-        className="react-flow__node-default"
-    >
-       
-    <video
-        controls  src={VideoLink}>
-    </video>
+  const [VideoLink, ChangeVideoLink] = useState(
+    "https://media.istockphoto.com/id/1413207061/video/road-traffic-in-delhi-roads.mp4?s=mp4-640x640-is&k=20&c=KnGos4ZVgHxZSV-zGAJk0mWsjR2kLGumoVcKI-PanEw="
+  );
 
-    <div 
-            onClick={(e) =>{RedirectClick(e)}}  
-            style={{height:"20px",bottom:"-50px",backgroundColor:'#bbd'}}
+  function RedirectClick(e) {
+    const HiddenInput = e.target.querySelector("input");
+    if (HiddenInput) {
+      HiddenInput.click();
+    }
+  }
+  function InputAdition(e) {
+    const InputVideoURL = URL.createObjectURL(e.target.files[0]);
+    ChangeVideoLink(InputVideoURL);
+  }
+  return (
+    <div
+      style={{ justifyContent: "center", width: "300px", height: "175px" }}
+      className="react-flow__node-default"
     >
-            <p>Click To Input</p>
-    <input 
-        hidden 
-        onChange={(e)=>{InputAdition(e)}}  
-        type="file" 
-        className="videoInput" 
-        accept="video/*"  />
-        
+      <video controls src={VideoLink}></video>
+
+      <div
+        onClick={(e) => {
+          RedirectClick(e);
+        }}
+        style={{ height: "20px", bottom: "-50px", backgroundColor: "#bbd" }}
+      >
+        <p>Click To Input</p>
+        <input
+          hidden
+          onChange={(e) => {
+            InputAdition(e);
+          }}
+          type="file"
+          className="videoInput"
+          accept="video/*"
+        />
+      </div>
+
+      <DivHandle
+        type="source"
+        id="1"
+        position={Position.Right}
+        style={{ top: "33%" }}
+        mediaType={MediaTypes.VIDEO}
+      />
+      <DivHandle
+        type="source"
+        id="2"
+        position={Position.Right}
+        style={{ top: "66%" }}
+        mediaType={MediaTypes.AUDIO}
+      />
     </div>
-    
-    
-    <DivHandle type="source" id={"VideoOutput"} position= {Position.Right} style={{top:"33%"}} mediaType = {MediaTypes.VIDEO} />
-    <DivHandle type="source" id={"audioOutput"} position= {Position.Right} style={{top:"66%"}} mediaType = {MediaTypes.AUDIO}  />
-    </div>)
+  );
 }
 
-
-
-export const nodeTypes  = {
-    "Output":VideoOutput,
-    "Difference":VideoDifference,
-    "Input":VideoInput
-}
+export const nodeTypes = {
+  Output: VideoOutput,
+  Difference: VideoDifference,
+  Input: VideoInput,
+};
 
 /// THis should be the ONLY place where the names are hardcoded
-export const  InitalNodes = [
-    { id: "a", type: "Input", position: { x: 10, y: 50 }, data: { }},
-    { id: "d", type: "Input", position: { x: 10, y: 270 }, data: { }},
-    { id: "b", type: "Difference", position: { x: 365, y: 150 }, data: { }},
-    { id: "c", type: "Output", position: { x: 370, y: 390 }, data: { }},
-]
+export const InitalNodes = [
+  { id: "a", type: "Input", position: { x: 10, y: 50 }, data: {} },
+  { id: "d", type: "Input", position: { x: 10, y: 270 }, data: {} },
+  { id: "b", type: "Difference", position: { x: 365, y: 150 }, data: {} },
+  { id: "c", type: "Output", position: { x: 370, y: 390 }, data: {} },
+];
